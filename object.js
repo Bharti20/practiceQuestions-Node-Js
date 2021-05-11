f86a9acdb1ffbe61ec2a49ef9aacbeb770b0353c
Bharti kumari <bhartikumari20@navgurukul.org>
2021-05-11 10:49:45 +0530

    addmethod inside object and new object

--- object.js
+++ object.js
@@ -1,7 +1,22 @@
 var person={
     name:"Bharti",
+    lastName:"kumari",
     age:23,
-    from:"Bihar"
+    from:"Bihar",
+    hobbies:["watching movie", "dance", "cooking"],
+    living:{
+        city:"kishanganj",
+        country:"India"
+    },
+    salary:function(){
+        return "nothing"
+    },
+    fullName:function(){
+        return this.name + " " + this.lastName;
+    }
+
 } 
 console.log(person["name"]) // can access using this method 
-console.log(person.name) // or can use this method 
+console.log(person.age) // dot-notation
+console.log(person.living.city)
+console.log(person.fullName())
