06eb088939dbbc1fee20d2302521555ded50cd4c
Bharti kumari <bhartikumari20@navgurukul.org>
2021-05-15 14:19:14 +0530

    practice switch statement

---
+++ switchStatments.js
@@ -0,0 +1,14 @@
+const dish= "Biryani"
+switch(dish){
+    case "dal rice":
+        console.log("correct")
+        // break;
+    case "chiken biryani":
+        console.log("not same dish")
+        // break;
+    case "Biryani":
+        console.log("yehhhh time to eat")
+        // break;
+    default:
+        console.log("try diffrent one")
+}
